7d6bf67933053f12cc787fb94df093ad3b6c62a6
OASans <1075611946@qq.com>
2019-05-15 14:37:38 +0800

    add destiny suggestion

--- miniprogram/pages/newRequest/newRequest.js
+++ miniprogram/pages/newRequest/newRequest.js
@@ -16,6 +16,8 @@ Page({
     openid: "",
     longitude: 116.4965075,
     latitude: 40.006103,
+    destiny:"",
+    suggestion:[],
   },
 
   /**
@@ -137,6 +139,7 @@ Page({
         address:this.data.address,
         longitude:this.data.longitude,
         latitude:this.data.latitude,
+        destiny:this.data.destiny,
         //_openid: this.openid
       },
       success: res => {
@@ -160,6 +163,54 @@ Page({
     wx.navigateTo({
       url: '/pages/newRequest/waitingUmbrella/waitingUmbrella',
     })
+  },
+
+  //数据回填方法
+  backfill: function (e) {
+    var id = e.currentTarget.id;
+    for (var i = 0; i < this.data.suggestion.length; i++) {
+      if (i == id) {
+        this.setData({
+          destiny: this.data.suggestion[i].title,
+          suggestion:[]
+        });
+      }
+    }
+  },
+
+  //触发关键词输入提示事件
+  getsuggest: function (e) {
+    var _this = this;
+    //调用关键词提示接口
+    qqmapsdk.getSuggestion({
+      //获取输入框值并设置keyword参数
+      keyword: e.detail.value, //用户输入的关键词，可设置固定值,如keyword:'KFC'
+      //region:'北京', //设置城市名，限制关键词所示的地域范围，非必填参数
+      success: function (res) {//搜索成功后的回调
+        console.log(res);
+        var sug = [];
+        for (var i = 0; i < res.data.length; i++) {
+          sug.push({ // 获取返回结果，放到sug数组中
+            title: res.data[i].title,
+            id: res.data[i].id,
+            addr: res.data[i].address,
+            city: res.data[i].city,
+            district: res.data[i].district,
+            latitude: res.data[i].location.lat,
+            longitude: res.data[i].location.lng
+          });
+        }
+        _this.setData({ //设置suggestion属性，将关键词搜索结果以列表形式展示
+          suggestion: sug
+        });
+      },
+      fail: function (error) {
+        console.error(error);
+      },
+      complete: function (res) {
+        console.log(res);
+      }
+    });
   }
 })
 
